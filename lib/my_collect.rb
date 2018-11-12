99f76ea91b2c35ca16eeff790cddce13177a9fe6
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-12 19:11:25 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   empty_array = []
   while i < collection.length 
-    yield collection[i]
+    empty_array << yield collection[i]
     i += 1 
   end
   empty_array

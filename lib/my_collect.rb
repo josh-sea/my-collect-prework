534fade4abe6c60074f84f2645689db8a8ff1a44
Joshua Cocciardi <joshua.cocciardi@gmail.com>
2018-11-12 19:04:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,6 @@
 def my_collect(collection)
-  i = 0 
+  i = 0
+  new_collection = []
   while i < collection.length 
     yield collection[i]
     i += 1 
@@ -8,5 +9,5 @@ def my_collect(collection)
 end
 
 my_collect do |item|
- puts item
+ new_collection << item
 end
